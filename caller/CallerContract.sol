aeb2f5e8533e340d6d14fa553f4c7df32885d540
guinjgida <61534762+guinjgida@users.noreply.github.com>
2024-04-05 18:07:50 +0800

    Create CallerContract.sol

---
+++ caller/CallerContract.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.5.0;
+//1. Import from the "./EthPriceOracleInterface.sol" file
+import "./EthPriceOracleInterface.sol";
+contract CallerContract {
+  // 2. Declare `EthPriceOracleInterface`
+  EthPriceOracleInterface private oracleInstance;
+  address private oracleAddress;
+  function setOracleInstanceAddress (address _oracleInstanceAddress) public {
+    oracleAddress = _oracleInstanceAddress;
+    //3. Instantiate `EthPriceOracleInterface`
+    oracleInstance = EthPriceOracleInterface(oracleAddress);
+  }
+}
